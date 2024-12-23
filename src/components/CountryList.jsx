fa28e0f8729c7ae52b971fafca7628f74151b06e
Sowad <sowadkhan912@gmail.com>
2024-12-23 17:51:07 -0500

    use context API for state management

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -2,8 +2,11 @@ import styles from './CountryList.module.css';
 import CountryItem from './CountryItem';
 import Spinner from './Spinner';
 import Message from './Message';
+import { useCities } from '../context/CitiesContext';
 
-function CountriesList({ cities, isLoading }) {
+function CountriesList() {
+    const { cities, isLoading } = useCities();
+
     if (isLoading) return <Spinner />;
 
     if (!cities.length) return <Message message='Add your first city by cliking on a city on the map' />
